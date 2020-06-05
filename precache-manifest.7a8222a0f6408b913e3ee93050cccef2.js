7f50427d01288dffb8ed113c48dc18044a4d7ae6
praveen-pujar <pujarpraveen9@gmail.com>
2020-06-05 16:48:09 +0530

    Updates

---
+++ precache-manifest.7a8222a0f6408b913e3ee93050cccef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6255e9adb98ede27dca838f521de89",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "e3b40c74e151e9b5f576",
+    "url": "/keeper-app/static/css/main.7ed6b628.chunk.css"
+  },
+  {
+    "revision": "347f34f3feb05403cd38",
+    "url": "/keeper-app/static/js/2.c5e05b3c.chunk.js"
+  },
+  {
+    "revision": "e3b40c74e151e9b5f576",
+    "url": "/keeper-app/static/js/main.5cad00ca.chunk.js"
+  },
+  {
+    "revision": "3e51cece14caf272b60a",
+    "url": "/keeper-app/static/js/runtime-main.fb6dad69.js"
+  }
+]);
